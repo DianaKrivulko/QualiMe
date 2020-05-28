f37996cc6f8e1313db064265026d80eff3d8ab98
DianaKrivulko <56954259+DianaKrivulko@users.noreply.github.com>
2020-05-28 18:18:48 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,77 +1,58 @@
 import React from 'react';
-import AppNavigation from "./navigation/AppNavigation";
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from "./screens/HomeScreen";
+import ChooseResource from "./screens/ChooseResource";
+import TestyHelpPerson from "./screens/TestyHelpPerson";
+import HelpyChoosePerson from "./screens/HelpyChoosePerson";
+import TopicHelpySchool from "./screens/TopicHelpySchool";
+import TopicTestyStudent from "./screens/TopicTestyStudent";
+import SubtopicHelpyStudent from "./screens/SubtopicHelpyStudent";
+import SubTopicHelpyStudentDisplay from "./screens/SubTopicHelpyStudentDisplay";
+import TasksTestyStudent from "./screens/TasksTestyStudent";
+import TaskTestyStudentDisplay from "./screens/TaskTestyStudentDisplay";
 
-export default function App(){
-    return(
-        <AppNavigation/>
-    )
-}
-/*import {StyleSheet, Text, View} from 'react-native';
-import {ChooseResButtonTesty} from "./components/ChooseResButtonTesty";
-import {ChooseResButtonHelpy} from "./components/ChooseResourseButtonHelpy";
-import {NavigationPanel} from "./components/NavigationPanel";
-import {SchoolSection} from "./components/SchoolSection";
-import {StudentSection} from "./components/StudentSection";
+const Stack = createStackNavigator();
+function App() {
 
-export default function App(props) {
-    return (
-        <View style={styles.container}>
+            return (
+                <NavigationContainer>
+                    <Stack.Navigator>
+                        <Stack.Screen name="Home" component={HomeScreen}/>
+                        <Stack.Screen name="Choose" component={ChooseResource}/>
+                        <Stack.Screen name="Testy" component={TestyHelpPerson}/>
+                        <Stack.Screen name="Helpy" component={HelpyChoosePerson}/>
+                        <Stack.Screen name="TopicHelpy" component={TopicHelpySchool}/>
+                        <Stack.Screen name="TopicTestyStudent" component={TopicTestyStudent}/>
+                        <Stack.Screen name="SubtopicHelpyStudent" component={SubtopicHelpyStudent}/>
+                        <Stack.Screen name="SubtopicHelpyStudentDisplay" component={SubTopicHelpyStudentDisplay}/>
+                        <Stack.Screen name="TasksTestyStudent" component={TasksTestyStudent}/>
+                        <Stack.Screen name="TaskTestyStudentDisplay" component={TaskTestyStudentDisplay}/>
+                    </Stack.Navigator>
+                </NavigationContainer>
 
-            <NavigationPanel/>
-            <Text style={styles.text}>
-                Кто вы?
-            </Text>
-            <SchoolSection/>
-            <StudentSection/>
-            <StudentSection/>
+            );
+    }
+export default App;
 
-        </View>
+/*
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-    );
-
-
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: 'black'
-    },
-    text: {
-        marginTop: 70,
-        marginBottom:30,
-        alignItems: 'center',
-        justifyContent: 'center',
-        //  fontFamily: 'Oswald',
-        fontSize: 28,
-        color: 'white',
-    },
-
-});*/
-
-/*import React from 'react';
-import {StyleSheet, Text, View, ImageBackground, Image} from 'react-native';
-import {Button} from "react-native-web";
-import {LogoFirst} from "./components/LogoFirst";
-import {LoginButton} from "./components/LoginButton";
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from "./screens/HomeScreen";
-import ChooseResourse from "./screens/ChooseResourse";
-
-export default function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeScreen}/>
-                <Stack.Screen name="Choose" component={ChooseResourse}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-
-    );
-}
-
-const Stack = createStackNavigator();*/
-
-
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+*/
